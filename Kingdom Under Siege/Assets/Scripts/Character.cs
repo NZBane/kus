ad9fc489a1444ccc321958dc1e5590d2e63d9dd8
Roolyle <ogmudlong@gmail.com>
2018-08-24 16:38:06 +1200

    Ive added more characters and player movement

---
+++ Kingdom Under Siege/Assets/Scripts/Character.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//abstract class prevents assigning of this script on objects by mistake
+public abstract class Character : MonoBehaviour {
+
+    [SerializeField]
+    private float speed;
+    protected Vector2 direction; //protected = similar to private but allows inheritance access
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+    //virtual void update allows it to be overriden by another update
+	protected virtual void Update ()
+    {
+        Move();
+     }
+
+    public void Move()
+    {
+        //delta time = time that has pass after update (good for optimization)
+        transform.Translate(direction * speed * Time.deltaTime);
+
+    }
+}
---
+++ Kingdom Under Siege/Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+
+  
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	protected override void Update () {
+        GetInput();
+        base.Update();  //executes Character.cs update|| base = access inheritance
+        
+        
+
+        
+	}
+   
+
+    //player direction/movement control
+    private void GetInput()
+    {
+        //reset direction after every loop
+        direction = Vector2.zero; //Prevents incrementing speed every loop causing player to run faster and faster.
+        //GetKey = Hold key down
+        //GetKeyDown = Toggle mode
+        if(Input.GetKey(KeyCode.W))
+        {
+            direction += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector2.right;
+        }
+    }
+}
